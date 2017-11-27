21f4d96eb6dd83270b26ae23f65beda81b02fac1
Hunter Hsieh <hunter.hsieh@sjsu.edu>
2017-11-27 11:31:57 -0800

    delete everything

--- resources/views/includes/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Footer -->
-<footer class="py-5 bg-dark">
-    <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Gamazon.com 2017</p>
-    </div>
-    <!-- /.container -->
-</footer>
