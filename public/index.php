edfda4276a973080c49201c07029fa9abd25d671
Hunter Hsieh <hunter.hsieh@sjsu.edu>
2017-11-27 14:42:03 -0800

    debug

--- public/index.php
+++ public/index.php
@@ -54,7 +54,7 @@ $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
-
-$response->send();
 echo "index.php"; exit;
+$response->send();
+
 $kernel->terminate($request, $response);
